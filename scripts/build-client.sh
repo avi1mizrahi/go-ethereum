5ab8504db50b4f3ec33ad22e38542488b4db21f9
Avi Mizrahi <avi1mizrahi@gmail.com>
2022-04-10 12:51:17 +0300

    update building scripts

--- scripts/build-client.sh
+++ scripts/build-client.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
-cd ..
+GETH_PATH=`readlink -f ..`
+THIS_PATH=`readlink -f .`
+cd $GETH_PATH
 make geth
 sleep 2
-cd scripts
+cd $THIS_PATH
 rm -r $CLIENT_DIR
 sleep 2
 mkdir $CLIENT_DIR
 cd $CLIENT_DIR
-../../build/bin/geth --datadir linked-data --port=$PORT --maxpeers=$MAX_PEERS --ws --ws.port=$WS_PORT --syncmode "full" --http --http.port=$HTTP_PORT --http.api personal,eth,net,web3 --bootnodes $BOOTNODES > logs 2>&1 &
+${GETH_PATH}/build/bin/geth --datadir linked-data --port=$PORT --maxpeers=$MAX_PEERS --ws --ws.port=$WS_PORT --syncmode "full" --http --http.port=$HTTP_PORT --http.api personal,eth,net,web3 --bootnodes $BOOTNODES > logs 2>&1 &
 echo sleeping...
 sleep 5
 
-../../build/bin/geth attach linked-data/geth.ipc --exec 'personal.newAccount("hello")'
-../../build/bin/geth attach linked-data/geth.ipc --exec 'miner.start()'
+${GETH_PATH}/build/bin/geth attach linked-data/geth.ipc --exec 'personal.newAccount("hello")'
+${GETH_PATH}/build/bin/geth attach linked-data/geth.ipc --exec 'miner.start()'
 
 touch current_peers
 while true; do
-	../../build/bin/geth attach linked-data/geth.ipc --exec 'loadScript("../dump_peers.js")' >> current_peers
+	${GETH_PATH}/build/bin/geth attach linked-data/geth.ipc --exec 'loadScript("'${THIS_PATH}'/dump_peers.js")' >> current_peers
 	sleep 30
 done &
